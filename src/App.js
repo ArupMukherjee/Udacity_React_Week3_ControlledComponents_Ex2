373714100fee9e3c24cff4cd73f3979d3e8d96a6
root <mukherjeearup@gmail.com>
2020-05-25 16:03:59 +0000

    Added the Init module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import CreateItem from './CreateNewItem'
 import DeleteLastItem from './DeleteLastItem'
+import ItemList from './ItemList'
 import './App.css';
 
 class App extends React.Component {
@@ -46,10 +47,7 @@ class App extends React.Component {
 			onDeleteLastItem={this.handleDeleteLastItem}
 			buttonDisables={this.noItemsFound}
 		/>
-        <p className="items">Items</p>
-        <ol className="item-list">
-          {this.state.items.map((item, index) => <li key={index}>{item}</li>)}
-        </ol>
+	    <ItemList items={this.state.items} />
       </div>
     );
   }
